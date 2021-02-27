e52c02324b97abe4677857c6ae612bfa3da15205
santfernandez <santfernandez94@gmail.com>
2021-02-27 18:55:21 -0300

    ready for dpploy 89

--- backend/server.js
+++ backend/server.js
@@ -26,16 +26,16 @@ app.use('/api/upload', uploadRoutes)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-if(process.env.NODE_ENV ===  'production') {
-    app.use(express.static(path.join(__dirname, '/fronted/build')))
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
- 
-    app.get('/', (req, res) => {
-        res.send('API is running......')
-    })
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
 app.use(notFound)
